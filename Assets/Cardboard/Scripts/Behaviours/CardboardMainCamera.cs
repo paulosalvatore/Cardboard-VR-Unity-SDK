ea8f642c39c3aa834b964f66bfd347d0365af365
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-10 01:37:29 -0300

    Remove demo files and create main scene

--- Assets/Cardboard/Scripts/Behaviours/CardboardMainCamera.cs
+++ Assets/Cardboard/Scripts/Behaviours/CardboardMainCamera.cs
@@ -1,40 +1,41 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-using UnityEngine.Experimental.Rendering;
+﻿using UnityEngine;
 using UnityEngine.Rendering;
 
+// ReSharper disable once CheckNamespace
 namespace MobfishCardboard
 {
-    public class CardboardMainCamera: MonoBehaviour
+    public class CardboardMainCamera : MonoBehaviour
     {
-        //Only used in dontDestroyAndSingleton
-        private static CardboardMainCamera instance;
+        // Only used in dontDestroyAndSingleton
+        private static CardboardMainCamera _instance;
 
         [Header("Cameras")]
         [SerializeField]
         private Camera novrCam;
+
         [SerializeField]
         private Camera leftCam;
+
         [SerializeField]
         private Camera rightCam;
+
         [SerializeField]
         private GameObject vrCamGroup;
+
         [SerializeField]
         private GameObject novrCamGroup;
 
         [Header("Options")]
         [SerializeField]
         private bool defaultEnableVRView;
+
         [Tooltip(
             "Set this GameObject DontDestroyOnLoad and Singleton. If it's not needed or any parent GameObject already have DontDestroyOnLoad, disable it")]
         [SerializeField]
         private bool dontDestroyAndSingleton = true;
 
-        private RenderTextureDescriptor eyeRenderTextureDesc;
-        private bool overlayIsOpen;
+        private RenderTextureDescriptor _eyeRenderTextureDesc;
+        private bool _overlayIsOpen;
 
         private void Awake()
         {
@@ -42,14 +43,16 @@ namespace MobfishCardboard
 
             if (dontDestroyAndSingleton)
             {
-                if (instance == null)
+                if (_instance == null)
                 {
                     DontDestroyOnLoad(gameObject);
-                    instance = this;
+
+                    _instance = this;
                 }
-                else if (instance != this)
+                else if (_instance != this)
                 {
                     Destroy(gameObject);
+
                     return;
                 }
             }
@@ -60,11 +63,11 @@ namespace MobfishCardboard
             CardboardManager.SetVRViewEnable(defaultEnableVRView);
         }
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             RefreshCamera();
             CardboardManager.deviceParamsChangeEvent += RefreshCamera;
+
             SwitchVRCamera();
             CardboardManager.enableVRViewChangedEvent += SwitchVRCamera;
         }
@@ -79,8 +82,8 @@ namespace MobfishCardboard
         {
             SetupEyeRenderTextureDescription();
 
-            RenderTexture newLeft = new RenderTexture(eyeRenderTextureDesc);
-            RenderTexture newRight = new RenderTexture(eyeRenderTextureDesc);
+            var newLeft = new RenderTexture(_eyeRenderTextureDesc);
+            var newRight = new RenderTexture(_eyeRenderTextureDesc);
             leftCam.targetTexture = newLeft;
             rightCam.targetTexture = newRight;
 
@@ -89,8 +92,9 @@ namespace MobfishCardboard
 
         private void SetupEyeRenderTextureDescription()
         {
-            Vector2Int resolution = CardboardUtility.GetAdjustedScreenResolution();
-            eyeRenderTextureDesc = new RenderTextureDescriptor()
+            var resolution = CardboardUtility.GetAdjustedScreenResolution();
+
+            _eyeRenderTextureDesc = new RenderTextureDescriptor
             {
                 dimension = TextureDimension.Tex2D,
                 width = resolution.x / 2,
@@ -101,12 +105,12 @@ namespace MobfishCardboard
                 vrUsage = VRTextureUsage.OneEye
             };
 
-            #if UNITY_2019_1_OR_NEWER
+#if UNITY_2019_1_OR_NEWER
             eyeRenderTextureDesc.graphicsFormat = SystemInfo.GetGraphicsFormat(DefaultFormat.LDR);
+
             Debug.LogFormat("CardboardMainCamera.SetupEyeRenderTextureDescription(), graphicsFormat={0}",
                 eyeRenderTextureDesc.graphicsFormat);
-
-            #endif
+#endif
         }
 
         private void SwitchVRCamera()
@@ -124,37 +128,25 @@ namespace MobfishCardboard
 
             RefreshCamera_Eye(leftCam,
                 CardboardManager.projectionMatrixLeft, CardboardManager.eyeFromHeadMatrixLeft);
+
             RefreshCamera_Eye(rightCam,
                 CardboardManager.projectionMatrixRight, CardboardManager.eyeFromHeadMatrixRight);
-
-
-            // if (CardboardManager.deviceParameter != null)
-            // {
-            //     leftCam.transform.localPosition =
-            //         new Vector3(-CardboardManager.deviceParameter.InterLensDistance / 2, 0, 0);
-            //     rightCam.transform.localPosition =
-            //         new Vector3(CardboardManager.deviceParameter.InterLensDistance / 2, 0, 0);
-            // }
         }
 
         private static void RefreshCamera_Eye(Camera eyeCam, Matrix4x4 projectionMat, Matrix4x4 eyeFromHeadMat)
         {
             if (!projectionMat.Equals(Matrix4x4.zero))
+            {
                 eyeCam.projectionMatrix = projectionMat;
+            }
 
-            //https://github.com/googlevr/cardboard/blob/master/sdk/lens_distortion.cc
             if (!eyeFromHeadMat.Equals(Matrix4x4.zero))
             {
-                Pose eyeFromHeadPoseGL = CardboardUtility.GetPoseFromTRSMatrix(eyeFromHeadMat);
+                var eyeFromHeadPoseGL = CardboardUtility.GetPoseFromTRSMatrix(eyeFromHeadMat);
                 eyeFromHeadPoseGL.position.x = -eyeFromHeadPoseGL.position.x;
                 eyeCam.transform.localPosition = eyeFromHeadPoseGL.position;
                 eyeCam.transform.localRotation = eyeFromHeadPoseGL.rotation;
             }
         }
-
-        // Update is called once per frame
-        // void Update()
-        // {
-        // }
     }
 }
